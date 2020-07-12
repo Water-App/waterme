fc5554c086225192db679ef7c9c1c790be271c2a
ElChapoGustapo <jzavala2012@gmail.com>
2020-07-12 13:42:12 -0600

    fixed issue where some core data points could be null.

--- WaterMe/WaterMe/ViewController.swift
+++ WaterMe/WaterMe/ViewController.swift
@@ -20,17 +20,29 @@ class ViewController: UIViewController {
   
    
     @IBAction func enterWasPressed(_ sender: Any){
+        //this should delete everything in our core data model
+        // save this for some sort of delete button or possible reset
+        // this reset all values
         
+//        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "WaterPoint")
+//        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+//        let persistentContainer = (UIApplication.shared.delegate as! AppDelegate).persistentContainer
+//
+//        do {
+//            try persistentContainer.viewContext.execute(deleteRequest)
+//        } catch let error as NSError {
+//            print(error)
+//        }
     
         let context = appDelegate.persistentContainer.viewContext
         let entity = NSEntityDescription.entity(forEntityName: "WaterPoint", in: context)
         let newWater = NSManagedObject(entity: entity!, insertInto: context)
-        
+
         let insert:Int! = Int(intakeLabel.text!)
         newWater.setValue(insert, forKey: "intake")
         let d = Date()
         newWater.setValue(d, forKey: "created")
-        
+
         do
         {
             try context.save()
