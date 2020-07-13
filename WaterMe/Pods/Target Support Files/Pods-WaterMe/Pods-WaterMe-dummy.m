8e5015791074c166ca041c52fe800fc53166eaf3
ElChapoGustapo <jzavala2012@gmail.com>
2020-07-13 13:09:03 -0600

    updated backgrounds

---
+++ WaterMe/Pods/Target Support Files/Pods-WaterMe/Pods-WaterMe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WaterMe : NSObject
+@end
+@implementation PodsDummy_Pods_WaterMe
+@end
--- WaterMe/WaterMe/AppDelegate.swift
+++ WaterMe/WaterMe/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Macaw
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
